f116db685e830204e47bbeba24b0a75752693b0a
Elnara <elutic@gmail.com>
2016-09-23 00:05:25 +0300

    Add angular-sanitize.js for ng-bind-html, Add main logic of tests

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app',['ngRoute', 'mdl']);
+var app = angular.module('app',['ngRoute', 'mdl', 'ngSanitize']);
 
 app.config(function($routeProvider){
   $routeProvider
--- script/test.controller.js
+++ script/test.controller.js
@@ -1,4 +1,77 @@
-app.controller('Test', function () {
-  this.currentTest = currentTest;
-  this.tests = tests;
-});
+app.controller('Test',[ '$http', function ($http) {
+  
+  var self = this;
+    self.currentTest = currentTest,
+    self.zones = [],
+    self.isCompleted = false,
+    self.currentQuestion = 0,
+    self.currentZone = "red",
+    self.positiveAnswers = 0,
+    self.result = '';
+
+  var testUrl = '/data/tests/test.' + self.currentTest + '.json';
+
+  self.resetCurrentQuestion = function () {
+    return self.currentQuestion = 0;
+  };
+
+  self.currentResult = function () {
+    return self.result = self.zones[self.currentZone].result;
+  };
+
+  self.isSpecialQuestion = function () {
+    if ( !self.isCompleted && self.currentZone === 'additional' &&  self.zones[self.currentZone].questions[self.currentQuestion]["type"] === 'input' ) {
+      return true;
+    }
+  };
+
+  $http.get( testUrl ).success(function (data) {
+    self.zones = data;
+
+    self.question = function () {
+
+      if (!self.isCompleted) {
+        var question = self.zones[self.currentZone].questions[self.currentQuestion];
+        return self.currentZone === 'additional' ? question["question"] : question
+      }
+
+    }
+
+    self.testContinue = function (answer) {
+      answer === 'yes' ? ++self.positiveAnswers : null;
+      ++self.currentQuestion;
+
+      if ( self.currentQuestion === self.zones[self.currentZone].questions.length ) {
+
+        if (self.currentZone !== 'additional') {
+
+          if ( self.positiveAnswers >= self.zones[self.currentZone].positiveAnswers ) {
+
+            self.currentResult();
+            self.currentZone = "additional";
+            self.resetCurrentQuestion();
+
+          } else {
+            self.positiveAnswers = 0;
+            self.currentZone = self.zones[self.currentZone]["nextZone"];
+            self.resetCurrentQuestion();
+
+            if (self.currentZone === 'white') {
+              self.currentResult();
+              self.currentZone = "additional";
+            }
+          }
+
+        } else {
+          self.isCompleted = true;
+        }
+
+      }
+      
+    };
+    
+  });
+
+  
+
+}]);
