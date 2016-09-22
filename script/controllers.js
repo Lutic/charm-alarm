29f6ade1d69c962557ecf861e18feade51239f65
Elnara <elutic@gmail.com>
2016-09-22 15:42:09 +0300

    Move objects tests and helpCenters to json files and get data from them to the controllers

--- script/controllers.js
+++ script/controllers.js
@@ -1,9 +1,13 @@
-var currentTest;
+var currentTest, tests = [];
 
-app.controller('Main', function () {
+app.controller('Main', [ '$http', function ($http) {
 
   var self = this;
-  self.tests = tests;
+
+  $http.get('/data/tests.json').success(function (data) {
+    tests = data;
+    self.tests = tests;
+  });
 
   self.testChoice = function (id, status) {
     if (status === 'open') {
@@ -20,65 +24,21 @@ app.controller('Main', function () {
     return currentTest === id;
   };
 
-});
+}]);
 
 app.controller('Test', function () {
   this.currentTest = currentTest;
   this.tests = tests;
 });
 
-app.controller('Help', function () {
-  this.helpCenters = helpCenters;
-});
+app.controller('Help', [ '$http', function ($http) {
+  var help = this;
+  help.helpCenters = [];
 
-var helpCenters = [
-  {
-    title: 'Благотворительный Фонд «Безопасный дом»',
-    description: 'психологическая, медицинская, социальная помощь и консультации (можешь позвонить или написать смс)',
-    phone: '8 (926) 073-95-75'
-  },
-  {
-    title: 'Горячая линия Международной организации по Миграции',
-    description: 'экстренная помощь, консультации',
-    phone: '8 (800) 333-00-16'
-  },
-  {
-    title: 'Телефон доверия центра помощи пережившим сексуальное насилие «Сестры»',
-    description: 'психологическая помощь по телефону',
-    phone: '8 (499) 901-02-01'
-  },
-  {
-    title: '«Помощь рядом»',
-    description: 'психологическая помощь детям и подросткам онлайн',
-    phone: '8 (812) 380-30-92'
-  }
-];
+  $http.get('/data/help-centers.json').success(function (data) {
+    help.helpCenters = data;
+  });
 
-var tests = [
-  {
-    id: 0,
-    title: 'Насколько крут твой парень?!',
-    img: 'img/bg_test1.jpg',
-    description: 'Хочешь узнать, повезло ли тебе с бойфрендом, или ты достойна большего? Наш тест даст ответ на эти вопросы, а после прохождения ты получишь парочку полезных советов.'
-  },
-  {
-    id: 1,
-    title: 'Работа твоей мечты! Слишком хорошо, чтобы быть правдой?',
-    img: 'img/bg_test2.jpg',
-    description: ''
-  },
-  {
-    id: 2,
-    title: 'Крепки ли Ваши отношения?',
-    img: 'img/bg_test3.jpg',
-    description: ''
-  },
-  {
-    id: 3,
-    title: 'Насколько тебя принимают сверстники?',
-    img: 'img/bg_test4.jpg',
-    description: ''
-  }
-];
+}]);
 
 
